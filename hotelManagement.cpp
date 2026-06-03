629f03bd45977e59a80a7d1f05b788eba5eceedc
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-06-03 22:26:28 +0600

    hotel management login panel

--- hotelManagement.cpp
+++ hotelManagement.cpp
@@ -1,16 +1,43 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
 class Admin{
     string username;
     string password;
     public:
+    Admin() {}
     Admin(string u, string p):username(u), password(p) {}
-    bool login(string u, string p){
-        return (u == username && p == password);
+
+    void input(){
+        cout<<"Enter username: ";
+        cin>>username;
+        cout<<"Enter password: ";
+        cin>>password;
     }
+
+    int login(){
+        ifstream file("login.txt");
+        if(!file.is_open()){
+            cout<<"Error opening file!"<<endl;
+            return 0;
+        }
+        string file_username, file_password;
+        while(file>>file_username>>file_password){
+            cout << "Read from file: " << file_username << " " << file_password << endl;
+            if(file_username == username && file_password == password){
+                cout<<"Login successful!"<<endl;
+                return 1;
+            }
+        }
+        cout<<"Login failed!"<<endl;
+        return 0;
+    }
+    
 };
 int main(){
-     
+    Admin admin;
+    admin.input();
+    admin.login();
     return 0;
 }
